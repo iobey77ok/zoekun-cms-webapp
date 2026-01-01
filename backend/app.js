8d2377f053579b0fc9296c1f06eb96de3a1d20d1
Beyyy <nattadonx@gmail.com>
2026-01-01 22:38:44 +0700

    Connect to mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
-const express = require('express');
-const app = express();
+require('dotenv').config()
 
+const express = require('express');
+const mongoose = require("mongoose");
+
+const app = express();
 app.use(express.json());
+
+mongoose.set("strictQuery", false);
+
+// Define the database URL to connect to.
+const uri = process.env.MONGODB_URI;
+
+mongoose.connect(uri)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
 
 app.get('/status', (req, res) => {
   res.json({
@@ -11,4 +24,4 @@ app.get('/status', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
